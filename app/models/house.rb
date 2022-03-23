f621c78c5318607f5d78fbbde8f7e046ebd99eca
Arseniy Chirkov <arsenii.chirkov@noveogroup.com>
2022-03-23 13:44:28 +0300

    Add validations to name country and city of house

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class House < ApplicationRecord
+  validates(:name, :country, :city, presence: true)
   validates(:price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999_999.99 })
-
 end
--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -2,22 +2,38 @@
 
 RSpec.describe HousesController, type: :controller do
   describe '#Create' do
-    context 'validates price' do
-      it 'doesnt create house with price less than 0 and 10 billions' do
-        post(:create, params: { house: { price: -1 } })
+    context 'validates fields' do
+      it 'doesnt create house with empty name' do
+        post(:create, params: { house: { country: FFaker::Address.country, city: FFaker::Address.city, price: 100 } })
+        expect(response.status).to(eq(422))
+      end
 
+      it 'doesnt create house with empty country' do
+        post(:create, params: { house: { name: FFaker::Name.name, city: FFaker::Address.city, price: 100 } })
+        expect(response.status).to(eq(422))
+      end
+
+      it 'doesnt create house with empty city' do
+        post(:create, params: { house: { name: FFaker::Name.name, country: FFaker::Address.country, price: 100 } })
+        expect(response.status).to(eq(422))
+      end
+
+      it 'doesnt create house with price less than 0' do
+        post(:create, params: { house: { name: FFaker::Name.name, country: FFaker::Address.country, city: FFaker::Address.city, price: -1 } })
         expect(response.status).to(eq(422))
       end
 
       it 'doesnt create house with price higher than or equal to 10 billions' do
-        post(:create, params: { house: { price: 9_999_999_999.991 } })
-
+        post(
+          :create, params: {
+            house: { name: FFaker::Name.name, country: FFaker::Address.country, city: FFaker::Address.city, price: 9_999_999_999.991 }
+          }
+        )
         expect(response.status).to(eq(422))
       end
 
-      it 'saves house with price in allowed range' do
-        post(:create, params: { house: { price: 100 } })
-
+      it 'saves house with valid fields' do
+        post(:create, params: { house: { name: FFaker::Name.name, country: FFaker::Address.country, city: FFaker::Address.city, price: 100 } })
         expect(response).to(redirect_to(assigns(:house)))
       end
     end
