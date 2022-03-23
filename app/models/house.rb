c3b9f5f3c1d5e683a537d5f92dcf1b026e2f6de9
Arseniy Chirkov <arsenii.chirkov@noveogroup.com>
2022-03-23 16:30:14 +0300

    Add validations and house booking controller

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class House < ApplicationRecord
+  has_many :house_bookings, dependent: :destroy
+
   validates(:name, :country, :city, presence: true)
   validates(:price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999_999.99 })
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: 'houses#index'
 
   resources :houses
+  resources :house_bookings, only: %i[create destroy]
 end
